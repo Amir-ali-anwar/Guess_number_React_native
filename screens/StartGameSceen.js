71b3c7ac6b37223c74c3ecc78441124aa86bc7f4
Amir Ali Anwar <amiralianwar611@gmail.com>
2022-11-11 20:58:04 +0500

    working on adding components

--- screens/StartGameSceen.js
+++ screens/StartGameSceen.js
@@ -1,18 +1,20 @@
-import React from 'react'
-import {View,Text,StyleSheet} from 'react-native'
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
 const StartGameSceen = (props) => {
   return (
-    <View style={styles.screen}><Text style={styles.text}>StartGameSceen</Text></View>
-  )
-}
-const styles= StyleSheet.create({
-    screen:{
-        flex:1,
-        padding:10,
-        alignItems:'center',
-    },
-    text:{
-        color:'#000',
-    }
-})
-export default StartGameSceen
+    <View style={styles.screen}>
+      <Text style={styles.text}>StartGameSceen</Text>
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 10,
+    alignItems: "center",
+  },
+  text: {
+    color: "#000",
+  },
+});
+export default StartGameSceen;
