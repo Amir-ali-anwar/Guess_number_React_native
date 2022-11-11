992a83d656f90fbf542ea3c83c6da12cd6e0f9b8
Amir Ali Anwar <amiralianwar611@gmail.com>
2022-11-11 20:54:40 +0500

    inital app setup

--- App.js
+++ App.js
@@ -1,20 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import Header from './components/Header.js'
+import StartGameSceen from './screens/StartGameSceen.js'
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Header title="Guess a number" />
+      <StartGameSceen />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+ 
 });
